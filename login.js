b814f27a8008fe225005bf8de65e766f3d7f7043
webpromo <rank_booster@yahoo.com>
2018-12-12 11:18:47 -0700

    Registration.html set up

--- login.js
+++ login.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
   res.sendFile('./ui/login.html',{ root:__dirname })
 })
+app.get('/register.html',function(req,res){
+  res.sendFile('./ui/register.html',{ root:__dirname })
+})
 
 app.listen(3210)
 console.log("Listening intently to port 3210");
